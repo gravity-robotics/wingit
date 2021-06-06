3a53e814c1ec3bd0cba30e523683c42987f7dd79
Arpit Chauhan <carpit680@gmail.com>
2021-06-07 03:34:03 +0530

    initial file creation

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.static("public"))
+
+
+app.get("/", function (req,res){
+    res.sendFile(__dirname + "/menu.html")
+})
+
+app.listen(3000, function (){
+    console.log("Server is now running at port 3000.")
+})
