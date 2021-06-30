f4cad5d8eea4c6a9d98f785ae887b9a28495c451
Arpit Chauhan <carpit680@gmail.com>
2021-06-30 17:25:56 +0530

    added login page

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-app.use( bodyParser.urlencoded({ extended: true }));
+app.use( bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {res.render('index')})
 app.get("/demo", (req, res) => {res.render('demo')})
 app.get("/create", (req, res) => {res.render('create')})
+app.get("/login", (req, res) => {res.render('login')})
+
+app.post('/login', (req, res) => {
+  // Insert Login Code Here
+  let username = req.body.username;
+  let password = req.body.password;
+  res.send(`Username: ${username} Password: ${password}`);
+});
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
