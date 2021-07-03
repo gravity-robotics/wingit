eb12852253c88d39b3f52ec834fa2b2125c3008b
Arpit Chauhan <carpit680@gmail.com>
2021-07-03 07:23:26 +0530

    login form validation and sanitization

--- app.js
+++ app.js
@@ -1,26 +1,68 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.use( bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {res.render('index')})
-app.get("/demo", (req, res) => {res.render('demo')})
-app.get("/create", (req, res) => {res.render('create')})
-app.get("/admin", (req, res) => {res.render('login')})
-
-app.post('/login', (req, res) => {
-  // Insert Login Code Here
-  let username = req.body.username;
-  let password = req.body.password;
-  res.send(`Username: ${username} Password: ${password}`);
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/demo", (req, res) => {
+  res.render("demo");
+});
+app.get("/create", (req, res) => {
+  res.render("create");
+});
+app.get("/admin", (req, res) => {
+  res.render("login");
 });
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+// Validation Array
+var loginValidate = [
+  // Check Username
+  check("email", "Please enter a valid Email Address")
+    .isEmail()
+    .trim()
+    .escape()
+    .normalizeEmail(),
+
+  // Check Password
+  check("password")
+    .trim()
+    .escape()
+    .isLength({ min: 8 })
+    .withMessage("Password Must Be at Least 8 Characters"),
+  // .matches("[0-9]")
+  // .withMessage("Password Must Contain a Number")
+  // .matches("[A-Z]")
+  // .withMessage("Password Must Contain an Uppercase Letter")
+];
+
+app.post("/admin", loginValidate, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array()[0].msg });
+  } else {
+    let username = req.body.username;
+    let password = req.body.password;
+    if (req.body.button == "login") {
+      res.send(`Email ID: ${email} Password: ${password}`);
+    } else if (req.body.button == "signup") {
+      res.render("coming-soon");
+    }
+  }
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
