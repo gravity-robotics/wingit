aeac77249ec2a61efb61fd1faff73aa467c8a9da
Arpit Chauhan <carpit680@gmail.com>
2021-08-02 22:37:40 +0530

    routing fixeed

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log(
     "Express server listening on port %d in %s mode",
