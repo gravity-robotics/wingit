99a67370986b57e7e61ff80af1f9b40ef401f086
Arpit Chauhan <carpit680@gmail.com>
2021-08-04 00:08:07 +0530

    color theme

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -6,9 +6,9 @@ function Plans() {
     <section className="container-fluid" id="plans">
       <div className="plans-div">
         <div className="plans-card-div">
-          <PricingCard title="Basic" price="0.00" features="Esse aliquip ut irure aliqua mollit incididunt veniam veniam." color1="#f08fee" color2="#f35d7b" />
+          <PricingCard title="Basic" price="0.00" features="Esse aliquip ut irure aliqua mollit incididunt veniam veniam." color1="#8ec6ff" color2="#7064be" />
           <PricingCard title="Standard" price="1000" features="Esse aliquip ut irure aliqua mollit incididunt veniam veniam." color1="#6bdbd4" color2="#00a0ff" />
-          <PricingCard title="Premium" price="3000" features="Esse aliquip ut irure aliqua mollit incididunt veniam veniam." color1="#8ec6ff" color2="#7064be" />
+          <PricingCard title="Premium" price="3000" features="Esse aliquip ut irure aliqua mollit incididunt veniam veniam."  color1="#f08fee" color2="#f35d7b" />
         </div>
       </div>
     </section>
