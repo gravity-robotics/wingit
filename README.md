2f1d49c4313a5a1fb6ebe1f19652ebbf121d8f5f
Arpit Chauhan <carpit680@gmail.com>
2021-08-03 23:54:20 +0530

    color theme for pricing cards

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Wingit.Menu
 ## Ordering made easy.
+
+   // "start": "node server",
+   Add this line to package.json before heroku push.
--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import "../css/style_pricing_card.css";
 
 function PricingCard(props) {
+  var bg = {background: "linear-gradient(" + props.color1 + "," +  props.color2 + ")"};
+  var titleBg = {backgroundColor: props.color1};
   return (
     <div className="pricingCard">
-      <div className="pricingCardTitleDiv">
+      <div className="pricingCardTitleDiv" style={titleBg}>
         <h1 className="pricingCardTitle">{props.title}</h1>
       </div>
       <div className="pricingDiv">
         <h1 className="price">&#x20B9; {props.price}</h1>
         <p className="month">/month</p>
       </div>
-      <div className="pricingFeatureseDiv" >
+      <div className="pricingFeatureseDiv" style={bg} >
         <p className="pricingCardFeatures">{props.features}</p>
         <p className="pricingCardFeatures">{props.features}</p>
         <p className="pricingCardFeatures">{props.features}</p>
